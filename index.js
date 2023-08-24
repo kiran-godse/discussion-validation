260e6034497d7ef6fe73b59634d694f62873b19e
Swapnil Gawali <112171267+Swapnil-Devops@users.noreply.github.com>
2023-08-24 12:53:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 
 try {
-  const commentBody = github.context.payload.comment.body;
+  // const commentBody = github.context.payload.comment.body;
   const discussionId = github.context.payload.discussion.id;
   const discussionNodeId = github.context.payload.discussion.node_id;
   const discussionBody = github.context.payload.discussion.body;
 
-  console.log(`Comment Body: ${commentBody}`);
+  // console.log(`Comment Body: ${commentBody}`);
   console.log(`Discussion ID: ${discussionId}`);
   console.log(`Discussion Node ID: ${discussionNodeId}`);
   console.log(`Discussion Body: ${discussionBody}`);
